7c9a603126baac7c98b4cb45f4a9295ef043f658
CJ <cj@cjandren.com>
2019-04-04 13:35:03 -0400

    Add project files.

---
+++ CJsConsole/CJsConsole.cpp
@@ -0,0 +1,58 @@
+// CJsConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+
+	std::string mine;
+	std::string option;
+	float price = 0;
+	int userchoice;
+	int quantity = 0;
+	int keepRunning;
+
+	//We only need to introduce the program once so it's outside the main loop
+	std::cout << "\nThis is my test program.\nIt will grow as I learn!\n";
+	std::cout << "This is CJs Test Program.";
+	//Put my learning things between here.
+
+
+	do {
+		//User menu to try out my different programs inside my program
+		std::cout << "\n\n\t----------Menu----------\n";
+		std::cout << "\tPress 1 to use the price calculator\n";
+		std::cout << "\tPress 2 to exit\n";
+		std::getline(std::cin, option);
+		std::stringstream(option) >> userchoice;
+
+		if (userchoice == 1) {
+			//Ask user for price of product to calculate
+			std::cout << "\tEnter price: ";
+			std::getline(std::cin, mine);
+			std::stringstream(mine) >> price;
+
+			//Ask user for quantity of product
+			std::cout << "\tEnter quantity: ";
+			std::getline(std::cin, mine);
+			std::stringstream(mine) >> quantity;
+			
+			//Results printed
+			std::cout << "\tTotal price: " << price * quantity << std::endl;
+			
+			keepRunning = 1;
+		}
+		else if (userchoice == 2) {
+			keepRunning = 0;
+		}
+		else {
+			keepRunning = 1;
+		}
+	}
+	//This stays until I'm done learning console apps
+		while (keepRunning == 1);
+	return 0;
+}
